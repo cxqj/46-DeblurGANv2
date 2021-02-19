7b9b4c1e5f14f4eee23de032c7c72551c8e2d0a7
cxqj <43702702+cxqj@users.noreply.github.com>
2021-02-19 20:49:31 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -15,10 +15,11 @@ from tqdm import tqdm
 
 import aug
 
-
+# 返回采样后的图片对
 def subsample(data: Iterable, bounds: Tuple[float, float], hash_fn: Callable, n_buckets=100, salt='', verbose=True):  # bounds:(0,0.9)
-    data = list(data)  # [(path_a,path_b),(path_a,path_b),....(path_a,path_b)]  300x2
-    buckets = split_into_buckets(data, n_buckets=n_buckets, salt=salt, hash_fn=hash_fn)  # (300,)  [46,61,30,35,....,25,96]??
+    data = list(data)  # [(path_a,path_b),(path_a,path_b),....(path_a,path_b)]  2103x2
+    # 将图片对编号随机置为0-100的整数
+    buckets = split_into_buckets(data, n_buckets=n_buckets, salt=salt, hash_fn=hash_fn)  # (2103,)  [46,61,30,35,....,25,96]??
 
     lower_bound, upper_bound = [x * n_buckets for x in bounds]  # 0, 90.0
     msg = f'Subsampling buckets from {lower_bound} to {upper_bound}, total buckets number is {n_buckets}'
@@ -28,7 +29,7 @@ def subsample(data: Iterable, bounds: Tuple[float, float], hash_fn: Callable, n_
         logger.info(msg)
     return np.array([sample for bucket, sample in zip(buckets, data) if lower_bound <= bucket < upper_bound])  # samples between 0-90
 
-
+# 随机生成hash地址
 def hash_from_paths(x: Tuple[str, str], salt: str = '') -> str:
     path_a, path_b = x
     names = ''.join(map(os.path.basename, (path_a, path_b)))  # 000047.png000047.png
@@ -70,7 +71,7 @@ class PairedDataset(Dataset):
         self.normalize_fn = normalize_fn
         logger.info(f'Dataset has been created with {len(self.data_a)} samples')
 
-        if preload:
+        if preload:  # 是否预加载图片对
             preload_fn = partial(self._bulk_preload, preload_size=preload_size)
             if files_a == files_b:
                 self.data_a = self.data_b = preload_fn(self.data_a)
@@ -117,18 +118,22 @@ class PairedDataset(Dataset):
     @staticmethod
     def from_config(config):
         config = deepcopy(config)
+        # 获取模糊和清晰所有图像的路径
         files_a, files_b = map(lambda x: sorted(glob(config[x], recursive=True)), ('files_a', 'files_b'))
+        # 图像增强
         transform_fn = aug.get_transforms(size=config['size'], scope=config['scope'], crop=config['crop'])
+        # 归一化操作
         normalize_fn = aug.get_normalize()
+        #裁剪函数
         corrupt_fn = aug.get_corrupt_function(config['corrupt'])
 
         hash_fn = hash_from_paths
         # ToDo: add more hash functions
-        verbose = config.get('verbose', True)
+        verbose = config.get('verbose', True)  # True
         data = subsample(data=zip(files_a, files_b),
                          bounds=config.get('bounds', (0, 1)),
                          hash_fn=hash_fn,
-                         verbose=verbose)   # (258,2)
+                         verbose=verbose)   # (1886,2)
 
         files_a, files_b = map(list, zip(*data))
 
